bb60d1c141f762bc268d1ad0330066ecb33ee481
Alexandre Cordeiro <alexandre.13a@gmail.com>
2026-04-21 10:26:11 -0300

    2026-04-21 new date

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -111,7 +111,7 @@ export default function Footer() {
                     <SvgIconWhatsApp />
                 </div>
                 <div className={style.divallrights}>
-                    <p className={style.allrights}>&copy; {new Date().getFullYear()} - Alexandre Cordeiro - All rights reserved</p>
+                    <p className={style.allrights}>&copy; { new Date().getFullYear() } - Alexandre Cordeiro - All rights reserved</p>
                 </div>
             </div>
         </div>
